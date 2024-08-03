ae0bf27b300335f79549ca42a69c0333f41acdec
Salahin <rocky.bgta@gmail.com>
2024-08-03 15:50:19 +0600

    remove deprecated HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/root/app.component';
-import { DemoComponent } from './components/demo/demo.component';
-import { FormComponent } from './components/form/form.component';
-import { FormsModule } from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './components/root/app.component';
+import {DemoComponent} from './components/demo/demo.component';
+import {FormComponent} from './components/form/form.component';
+import {FormsModule} from "@angular/forms";
+import {provideHttpClient} from "@angular/common/http";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,11 +19,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
     NgbModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient()
   ],
   bootstrap: [FormComponent]
 })
